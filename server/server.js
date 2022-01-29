05cbaf7717e8d78b8fe8d9bc288be1a3baa8fdbb
saifali-95 <saifali7646@gmail.com>
2022-01-29 13:19:24 -0500

    add socket connection

--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
 const server = app.listen(3000);
+const io = socket(server);
 
 app.use(express.static('../client'));
+
+const newConnection = function(){
+  console.log('socket');
+}
+
+io.sockets.on('connection', newConnection);
+
+
+
 
 console.log('Server is running');
